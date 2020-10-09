2b7a394fb69b2f9819c7fae2a4bdc9f04a73dc1f
Snubb <60389050+Snubb@users.noreply.github.com>
2020-10-09 11:14:33 +0200

    Initial commit

---
+++ src/Hellooo.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Hellooo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        JOptionPane.showMessageDialog(null,"Hello World");
+    }
+}
